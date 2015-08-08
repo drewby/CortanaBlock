95598e9dc1ecaf8f4aa5991aaa108f44716ef566
drewby <drew@drewby.com>
2015-08-08 15:17:50 +0900

    Initial commit.

---
+++ minecraft/CortanaBlock/src/main/java/com/cortanamod/CortanaMod.java
@@ -0,0 +1,20 @@
+package com.cortanamod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = CortanaMod.MODID, version = CortanaMod.VERSION, name="Cortana Block")
+public class CortanaMod
+{
+    public static final String MODID = "cortanamod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
